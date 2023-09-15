aa6bb93513a5227777fab04070232e7e44b68ca7
junsuPark <krpjs0508@gmail.com>
2023-09-15 12:23:25 +0900

    ⚙️:: :core:core-database 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     repositories {
         google()
@@ -15,3 +17,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "XQUARE Android v2"
 include(":app")
+include(":core:core-database")
