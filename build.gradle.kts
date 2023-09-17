c574a529079ee5c0197d6663b16990d723e21d31
junsuPark <krpjs0508@gmail.com>
2023-09-18 08:31:04 +0900

    ⚙️:: kotlin serialization 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
 }
