e165eac92bcd447bbb37a11c606795f9913a8fe4
junsuPark <krpjs0508@gmail.com>
2023-09-18 17:09:10 +0900

    ⚙️:: ksp 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.ksp) apply false
 }
