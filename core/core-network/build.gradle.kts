6243dd5f731b36128f11890344445e352121ea37
junsuPark <krpjs0508@gmail.com>
2023-09-18 11:13:10 +0900

    ⚙️:: :core:core-network :shared: 의존성 추가

--- core/core-network/build.gradle.kts
+++ core/core-network/build.gradle.kts
@@ -41,4 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(project(":shared:shared-date"))
+    implementation(project(":shared:shared-alias"))
 }
