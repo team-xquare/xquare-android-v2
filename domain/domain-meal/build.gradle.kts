06f6549d2ba7a35437446bd7a08a61e53b3f82c4
junsuPark <krpjs0508@gmail.com>
2023-09-18 11:26:39 +0900

    ⚙️:: :domain:domain-meal 서브모듈 의존성 추가

--- domain/domain-meal/build.gradle.kts
+++ domain/domain-meal/build.gradle.kts
@@ -41,4 +41,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(project(":data:data-meal"))
+
+    implementation(project(":shared:shared-alias"))
+    implementation(project(":shared:shared-date"))
 }
