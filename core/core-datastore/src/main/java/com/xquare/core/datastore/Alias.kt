1ffaf6bd440f42ab9922b0364f07cd3706d94199
junsuPark <krpjs0508@gmail.com>
2023-09-21 20:04:24 +0900

    📑:: PreferencesDataStore typealias 추가

---
+++ core/core-datastore/src/main/java/com/xquare/core/datastore/Alias.kt
@@ -0,0 +1,6 @@
+package com.xquare.core.datastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+
+typealias PreferencesDataStore = DataStore<Preferences>
--- core/core-datastore/src/main/java/com/xquare/core/datastore/CoreDataStoreModule.kt
+++ core/core-datastore/src/main/java/com/xquare/core/datastore/CoreDataStoreModule.kt
@@ -1,12 +1,10 @@
 package com.xquare.core.datastore
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val coreDataStoreModule: Module
     get() = module {
-        single<DataStore<Preferences>> { androidContext().preferencesDataStore }
+        single<PreferencesDataStore> { androidContext().preferencesDataStore }
     }
