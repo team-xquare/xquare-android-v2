ada68d10b068be93bd852b448535bb5c0e258d2a
junsuPark <krpjs0508@gmail.com>
2023-09-21 13:38:18 +0900

    📑:: database MealEntity 추가

--- core/core-database/src/main/java/com/xquare/core/database/entity/MealEntity.kt
+++ core/core-database/src/main/java/com/xquare/core/database/entity/MealEntity.kt
@@ -3,11 +3,12 @@ package com.xquare.core.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.xquare.core.database.DatabaseEntity
 import com.xquare.shared.alias.Dish
 import org.joda.time.LocalDate
 
 @Entity(tableName = "tbl_meal")
-data class MealEntity(
+internal data class MealEntity(
     @PrimaryKey @ColumnInfo("date") val date: LocalDate,
     @ColumnInfo("breakfast") val breakfast: List<Dish>,
     @ColumnInfo("kcal_breakfast") val kcalOfBreakfast: List<Dish>,
@@ -15,4 +16,4 @@ data class MealEntity(
     @ColumnInfo("kcal_lunch") val kcalOfLunch: List<Dish>,
     @ColumnInfo("dinner") val dinner: List<Dish>,
     @ColumnInfo("kcal_dinner") val kcalOfDinner: List<Dish>,
-)
+) : DatabaseEntity
