73108a5358891f7aec39bddfca98819f406b14f0
junsuPark <krpjs0508@gmail.com>
2023-09-21 15:16:03 +0900

    ⚡️:: CoreDatabaseModule 구현

--- core/core-database/src/main/java/com/xquare/core/database/CoreDatabaseModule.kt
+++ core/core-database/src/main/java/com/xquare/core/database/CoreDatabaseModule.kt
@@ -1,12 +1,14 @@
 package com.xquare.core.database
 
 import androidx.room.Room
+import com.xquare.core.database.dao.meal.MealDao
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val coreDatabaseModule: Module
     get() = module {
+        includes(daoModule)
         single<XquareDatabase> {
             Room.databaseBuilder(
                 context = androidContext(),
@@ -14,4 +16,9 @@ val coreDatabaseModule: Module
                 name = "database-xquare",
             ).build()
         }
+    }
+
+private val daoModule: Module
+    inline get() = module {
+        single<MealDao> { get<XquareDatabase>().mealDao }
     }
