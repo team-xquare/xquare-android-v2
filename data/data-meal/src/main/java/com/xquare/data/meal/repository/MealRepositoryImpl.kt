3f7733b1500add27484bd6d5cef08335ff1d3110
junsuPark <krpjs0508@gmail.com>
2023-09-21 20:09:05 +0900

    ⚡️:: MealRepositoryImpl 구현

--- data/data-meal/src/main/java/com/xquare/data/meal/repository/MealRepositoryImpl.kt
+++ data/data-meal/src/main/java/com/xquare/data/meal/repository/MealRepositoryImpl.kt
@@ -1,13 +1,16 @@
 package com.xquare.data.meal.repository
 
 import com.xquare.data.meal.Meal
+import com.xquare.database.meal.datasource.MealDatabaseDataSource
+import com.xquare.datastore.meal.datasource.MealDataStoreDataSource
+import com.xquare.network.meal.datasource.MealNetworkDataSource
 import kotlinx.coroutines.flow.Flow
 import org.joda.time.LocalDate
 
 internal class MealRepositoryImpl(
-    private val databaseMealDataSource: DatabaseMealDataSource,
-    private val datastoreMealDataSource: DataStoreMealDataSource,
-    private val networkMealDataSource: NetworkMealDataSource,
+    private val mealDatabaseDataSource: MealDatabaseDataSource,
+    private val mealDataStoreDataSource: MealDataStoreDataSource,
+    private val mealNetworkDataSource: MealNetworkDataSource,
 ) : MealRepository() {
     override fun getMealOfDay(date: LocalDate): Flow<Meal> {
         TODO("Not yet implemented")
