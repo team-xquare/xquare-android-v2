c797b9e40b44b35d551f93176f21f11a8e82f04a
junsuPark <krpjs0508@gmail.com>
2023-09-22 07:47:49 +0900

    ⚡️:: String Type Converter 구현

---
+++ core/core-database/src/main/java/com/xquare/core/database/converter/ListTypeConverters.kt
@@ -0,0 +1,14 @@
+package com.xquare.core.database.converter
+
+import androidx.room.TypeConverter
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+internal class ListTypeConverters {
+
+    @TypeConverter
+    fun stringListToString(value: List<String>): String = Json.encodeToString(value)
+
+    @TypeConverter
+    fun stringToStringList(value: String): List<String> = Json.decodeFromString(value)
+}
