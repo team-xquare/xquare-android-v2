6d0a219e2a4ffa2a576c90cc0f68069db8df8330
junsuPark <krpjs0508@gmail.com>
2023-09-21 15:15:51 +0900

    ⚡️:: XquareDatabase 구현

--- core/core-database/src/main/java/com/xquare/core/database/XquareDatabase.kt
+++ core/core-database/src/main/java/com/xquare/core/database/XquareDatabase.kt
@@ -2,7 +2,7 @@ package com.xquare.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.xquare.core.database.dao.MealDao
+import com.xquare.core.database.dao.meal.MealDaoAbs
 import com.xquare.core.database.entity.MealEntity
 
 @Database(
@@ -11,6 +11,6 @@ import com.xquare.core.database.entity.MealEntity
     ],
     version = 0,
 )
-abstract class XquareDatabase : RoomDatabase() {
-    abstract val mealDao: MealDao
+internal abstract class XquareDatabase : RoomDatabase() {
+    abstract val mealDao: MealDaoAbs
 }
