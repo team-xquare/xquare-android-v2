4633f3318003cfa79b5e5d2a0e353edfe33b1750
junsuPark <krpjs0508@gmail.com>
2023-09-21 15:15:01 +0900

    📑:: MealDao 추가

---
+++ core/core-database/src/main/java/com/xquare/core/database/dao/meal/MealDao.kt
@@ -0,0 +1,12 @@
+package com.xquare.core.database.dao.meal
+
+import com.xquare.core.database.DatabaseDao
+import com.xquare.core.database.entity.MealEntity
+import kotlinx.coroutines.flow.Flow
+import org.joda.time.LocalDate
+
+abstract class MealDao : DatabaseDao<MealEntity>() {
+    abstract fun findByDate(date: LocalDate): Flow<MealEntity>
+    abstract fun findMonthOfMealsByDate(date: LocalDate): Flow<MealEntity>
+    abstract fun deleteByDate(date: LocalDate)
+}
