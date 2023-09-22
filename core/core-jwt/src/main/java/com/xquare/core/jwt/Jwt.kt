931e68d3aeebe71d0729402f0aff90df73bfeb99
junsuPark <krpjs0508@gmail.com>
2023-09-22 16:35:21 +0900

    📑:: Jwt 파일 선언

---
+++ core/core-jwt/src/main/java/com/xquare/core/jwt/Jwt.kt
@@ -0,0 +1,27 @@
+package com.xquare.core.jwt
+
+import org.koin.java.KoinJavaComponent.get
+
+val accessToken: AccessToken
+    inline get() = get(
+        clazz = String::class.java,
+        qualifier = JwtDiQualifier.ACCESS_TOKEN,
+    )
+
+val accessTokenExpiration: AccessTokenExpiration
+    inline get() = get(
+        clazz = String::class.java,
+        qualifier = JwtDiQualifier.ACCESS_TOKEN_EXPIRATION,
+    )
+
+val refreshToken: RefreshToken
+    inline get() = get(
+        clazz = String::class.java,
+        qualifier = JwtDiQualifier.REFRESH_TOKEN,
+    )
+
+val refreshTokenExpiration: RefreshTokenExpiration
+    inline get() = get(
+        clazz = String::class.java,
+        qualifier = JwtDiQualifier.REFRESH_TOKEN_EXPIRATION,
+    )
