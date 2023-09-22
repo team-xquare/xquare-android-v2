700c3e995dc146eb5d2292e05fd9f651ed6bd6d9
junsuPark <krpjs0508@gmail.com>
2023-09-22 16:47:04 +0900

    ⚡️:: JwtStoreImpl 구현

--- core/core-jwt/src/main/java/com/xquare/core/jwt/store/JwtStoreImpl.kt
+++ core/core-jwt/src/main/java/com/xquare/core/jwt/store/JwtStoreImpl.kt
@@ -1,7 +1,53 @@
 package com.xquare.core.jwt.store
 
+import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.xquare.core.datastore.PreferencesDataStore
+import com.xquare.core.jwt.AccessToken
+import com.xquare.core.jwt.AccessTokenExpiration
+import com.xquare.core.jwt.RefreshToken
+import com.xquare.core.jwt.RefreshTokenExpiration
+import com.xquare.core.jwt.exception.AccessTokenExpirationNotFoundException
+import com.xquare.core.jwt.exception.AccessTokenNotFoundException
+import com.xquare.core.jwt.exception.RefreshTokenExpirationNotFoundException
+import com.xquare.core.jwt.exception.RefreshTokenNotFoundException
+import com.xquare.shared.date.toLocalDateTime
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 internal class JwtStoreImpl(
     private val preferencesDataStore: PreferencesDataStore,
-) : JwtStore()
+) : JwtStore() {
+    override fun loadAccessToken(): Flow<AccessToken> =
+        preferencesDataStore.data.map { preferences ->
+            preferences[ACCESS_TOKEN] ?: throw AccessTokenNotFoundException()
+        }
+
+    override fun loadAccessTokenExpiration(): Flow<AccessTokenExpiration> =
+        preferencesDataStore.data.map { preferences ->
+            val longValue = preferences[ACCESS_TOKEN_EXPIRATION]
+                ?: throw AccessTokenExpirationNotFoundException()
+
+            return@map longValue.toLocalDateTime()
+        }
+
+    override fun loadRefreshToken(): Flow<RefreshToken> =
+        preferencesDataStore.data.map { preferences ->
+            preferences[REFRESH_TOKEN] ?: throw RefreshTokenNotFoundException()
+        }
+
+    override fun loadRefreshTokenExpiration(): Flow<RefreshTokenExpiration> =
+        preferencesDataStore.data.map { preferences ->
+            val longValue = preferences[REFRESH_TOKEN_EXPIRATION]
+                ?: throw RefreshTokenExpirationNotFoundException()
+
+            return@map longValue.toLocalDateTime()
+        }
+
+    private companion object {
+        val ACCESS_TOKEN = stringPreferencesKey("access-token")
+        val ACCESS_TOKEN_EXPIRATION = longPreferencesKey("access-token-expiration")
+        val REFRESH_TOKEN = stringPreferencesKey("refresh-token")
+        val REFRESH_TOKEN_EXPIRATION = longPreferencesKey("refresh-token-expiration")
+    }
+}
