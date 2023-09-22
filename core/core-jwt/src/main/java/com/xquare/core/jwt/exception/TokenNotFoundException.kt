c39de9364a32cbef52747b2261a0851d708de989
junsuPark <krpjs0508@gmail.com>
2023-09-22 10:20:37 +0900

    ⚡️:: AccessTokenNotFoundException, RefreshTokenNotFoundException 구현

--- core/core-jwt/src/main/java/com/xquare/core/jwt/exception/TokenNotFoundException.kt
+++ core/core-jwt/src/main/java/com/xquare/core/jwt/exception/TokenNotFoundException.kt
@@ -1,3 +1,7 @@
 package com.xquare.core.jwt.exception
 
-abstract class TokenNotFoundException : RuntimeException()
+sealed class TokenNotFoundException(message: String?) : RuntimeException(message)
+
+class AccessTokenNotFoundException : TokenNotFoundException("Access token not found")
+
+class RefreshTokenNotFoundException : TokenNotFoundException("Refresh token not found")
