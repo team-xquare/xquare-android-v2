eebe2bbb0308dee07db00dfac7718bd5f0b1ca7c
junsuPark <krpjs0508@gmail.com>
2023-09-22 09:01:57 +0900

    ⚡️:: LocalDate Util 구현

--- shared/shared-date/src/main/java/com/xquare/shared/date/DateUtils.kt
+++ shared/shared-date/src/main/java/com/xquare/shared/date/DateUtils.kt
@@ -2,6 +2,7 @@ package com.xquare.shared.date
 
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
+import org.joda.time.LocalDate
 import org.joda.time.LocalDateTime
 
 fun LocalDateTime.toEpochMillis(
@@ -11,3 +12,19 @@ fun LocalDateTime.toEpochMillis(
 fun Long.toLocalDateTime(
     zone: DateTimeZone = DateTimeZone.UTC,
 ): LocalDateTime = DateTime(this, zone).toLocalDateTime()
+
+fun LocalDate.toEpochMillis(
+    zone: DateTimeZone = DateTimeZone.UTC,
+): Long = DateTime(
+    this.year,
+    this.monthOfYear,
+    this.dayOfMonth,
+    0,
+    0,
+    0,
+    zone,
+).millis
+
+fun Long.toLocalDate(
+    zone: DateTimeZone = DateTimeZone.UTC,
+): LocalDate = LocalDate(this * 1000, zone)
