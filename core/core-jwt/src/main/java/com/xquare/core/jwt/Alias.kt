b80759dfda19e5bb7aed6c06a683f9a009b84f4a
junsuPark <krpjs0508@gmail.com>
2023-09-22 09:30:47 +0900

    📑:: Token Alias 구현

---
+++ core/core-jwt/src/main/java/com/xquare/core/jwt/Alias.kt
@@ -0,0 +1,11 @@
+package com.xquare.core.jwt
+
+import org.joda.time.LocalDateTime
+
+typealias AccessToken = String
+
+typealias AccessTokenExpiration = LocalDateTime
+
+typealias RefreshToken = String
+
+typealias RefreshTokenExpiration = LocalDateTime
