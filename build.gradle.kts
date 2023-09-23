486611aa488af5bca9019613d8e076a31337f315
junsuPark <krpjs0508@gmail.com>
2023-09-23 11:59:00 +0900

    ♻️:: ktlint 반영 gradle 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.kotlinSerialization) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.ktlint) apply true
+	alias(libs.plugins.androidApplication) apply false
+	alias(libs.plugins.androidLibrary) apply false
+	alias(libs.plugins.kotlinAndroid) apply false
+	alias(libs.plugins.kotlinSerialization) apply false
+	alias(libs.plugins.ksp) apply false
+	alias(libs.plugins.ktlint) apply true
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,17 @@
-@file:Suppress("UnstableApiUsage")
-
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+	repositories {
+		google()
+		mavenCentral()
+		gradlePluginPortal()
+	}
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { setUrl("https://jitpack.io") }
-    }
+	repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+	repositories {
+		google()
+		mavenCentral()
+		maven { setUrl("https://jitpack.io") }
+	}
 }
 
 rootProject.name = "XQUARE Android v2"
