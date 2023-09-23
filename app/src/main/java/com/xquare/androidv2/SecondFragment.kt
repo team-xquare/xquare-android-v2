047c53b9d49321c77fcdba6251688be4acecbf63
junsuPark <krpjs0508@gmail.com>
2023-09-23 17:00:06 +0900

    ♻️:: :app 모듈 리팩토링

--- app/src/main/java/com/xquare/androidv2/SecondFragment.kt
+++ app/src/main/java/com/xquare/androidv2/SecondFragment.kt
@@ -1,10 +1,10 @@
 package com.xquare.androidv2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.xquare.androidv2.databinding.FragmentSecondBinding
 
@@ -12,33 +12,34 @@ import com.xquare.androidv2.databinding.FragmentSecondBinding
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
+	private var _binding: FragmentSecondBinding? = null
 
-    private var _binding: FragmentSecondBinding? = null
+	// This property is only valid between onCreateView and
+	// onDestroyView.
+	private val binding get() = _binding!!
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?,
+	): View? {
+		_binding = FragmentSecondBinding.inflate(inflater, container, false)
+		return binding.root
+	}
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+	override fun onViewCreated(
+		view: View,
+		savedInstanceState: Bundle?,
+	) {
+		super.onViewCreated(view, savedInstanceState)
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
+		binding.buttonSecond.setOnClickListener {
+			findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+		}
+	}
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
 }
