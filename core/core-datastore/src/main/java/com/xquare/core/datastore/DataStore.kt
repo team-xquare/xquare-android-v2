cbc848fec5fd42e80130c46cda07f2c83bd94ea6
junsuPark <krpjs0508@gmail.com>
2023-09-23 17:00:49 +0900

    ♻️:: core-datastore 모듈 리팩토링

--- core/core-datastore/src/main/java/com/xquare/core/datastore/DataStore.kt
+++ core/core-datastore/src/main/java/com/xquare/core/datastore/DataStore.kt
@@ -3,4 +3,6 @@ package com.xquare.core.datastore
 import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
 
-internal val Context.preferencesDataStore: PreferencesDataStore by preferencesDataStore(name = "datastore-preferences-xquare")
+internal val Context.preferencesDataStore: PreferencesDataStore by preferencesDataStore(
+	name = "datastore-preferences-xquare",
+)
