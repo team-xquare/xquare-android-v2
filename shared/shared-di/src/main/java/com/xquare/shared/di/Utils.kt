9b45f0fe7f0107e93d0b1667d8eae80b2c27b05f
junsuPark <krpjs0508@gmail.com>
2023-09-23 17:03:31 +0900

    ♻️:: shared-di 모듈 리팩토링

--- shared/shared-di/src/main/java/com/xquare/shared/di/Utils.kt
+++ shared/shared-di/src/main/java/com/xquare/shared/di/Utils.kt
@@ -3,6 +3,7 @@ package com.xquare.shared.di
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-inline fun <reified T> get(): T = object : KoinComponent {
-    val value: T by inject()
-}.value
+inline fun <reified T> get(): T =
+	object : KoinComponent {
+		val value: T by inject()
+	}.value
