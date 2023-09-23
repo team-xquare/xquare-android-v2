85e5c4552f1aa5cc441c0c32331b84ebc43293f4
junsuPark <krpjs0508@gmail.com>
2023-09-23 17:01:45 +0900

    ♻️:: database-meal 모듈 리팩토링

--- database/database-meal/src/main/java/com/xquare/database/meal/datasource/MealDatabaseDataSourceImpl.kt
+++ database/database-meal/src/main/java/com/xquare/database/meal/datasource/MealDatabaseDataSourceImpl.kt
@@ -3,5 +3,5 @@ package com.xquare.database.meal.datasource
 import com.xquare.database.meal.service.MealDatabaseService
 
 internal class MealDatabaseDataSourceImpl(
-    private val mealDatabaseService: MealDatabaseService,
+	private val mealDatabaseService: MealDatabaseService,
 ) : MealDatabaseDataSource()
