b31603bf120cda33b27120107578209db2b79b84
junsuPark <krpjs0508@gmail.com>
2023-09-24 08:02:44 +0900

    ⚡️:: :data:data-user 의존성 수정

--- data/data-user/build.gradle.kts
+++ data/data-user/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 
 	implementation(libs.core.ktx)
 	implementation(libs.appcompat)
-	implementation(libs.material)
+
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.test.ext.junit)
 	androidTestImplementation(libs.espresso.core)
