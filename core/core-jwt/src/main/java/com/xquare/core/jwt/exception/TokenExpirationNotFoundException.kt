5ffdf6719819e35a6ce6f6aedb51ff6512ed297b
junsuPark <krpjs0508@gmail.com>
2023-09-23 17:01:06 +0900

    ♻️:: core-jwt 모듈 리팩토링

--- core/core-jwt/src/main/java/com/xquare/core/jwt/exception/TokenExpirationNotFoundException.kt
+++ core/core-jwt/src/main/java/com/xquare/core/jwt/exception/TokenExpirationNotFoundException.kt
@@ -3,7 +3,7 @@ package com.xquare.core.jwt.exception
 sealed class TokenExpirationNotFoundException(message: String?) : RuntimeException(message)
 
 class AccessTokenExpirationNotFoundException :
-    TokenExpirationNotFoundException("Access token expiration not found")
+	TokenExpirationNotFoundException("Access token expiration not found")
 
 class RefreshTokenExpirationNotFoundException :
-    TokenExpirationNotFoundException("Refresh token expiration not found")
+	TokenExpirationNotFoundException("Refresh token expiration not found")
