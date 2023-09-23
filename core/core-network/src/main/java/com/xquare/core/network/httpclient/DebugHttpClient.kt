01cc0ce62293379c5333da18780e3e8863c04b02
junsuPark <krpjs0508@gmail.com>
2023-09-23 17:01:13 +0900

    ♻️:: core-network 모듈 리팩토링

--- core/core-network/src/main/java/com/xquare/core/network/httpclient/DebugHttpClient.kt
+++ core/core-network/src/main/java/com/xquare/core/network/httpclient/DebugHttpClient.kt
@@ -4,9 +4,10 @@ import com.xquare.core.network.NetworkDiQualifier
 import io.ktor.client.HttpClient
 import org.koin.java.KoinJavaComponent.get
 
-internal val debugHttpClient = get<HttpClient>(
-    clazz = HttpClient::class.java,
-    qualifier = NetworkDiQualifier.HTTP_CLIENT_DEFAULT,
-).config {
-    // TODO: make debug client 
-}
+internal val debugHttpClient =
+	get<HttpClient>(
+		clazz = HttpClient::class.java,
+		qualifier = NetworkDiQualifier.HTTP_CLIENT_DEFAULT,
+	).config {
+		// TODO: make debug client
+	}
