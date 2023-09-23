7a179f71b808a1e849c36d0686e7618af4503a97
junsuPark <krpjs0508@gmail.com>
2023-09-23 11:18:22 +0900

    ⚡️:: ktlint 플러그인 의존성 선언

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.ktlint) apply true
 }
