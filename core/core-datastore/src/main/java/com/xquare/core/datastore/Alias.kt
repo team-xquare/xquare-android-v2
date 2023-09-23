5a10da5aa5b39c04db61a3afb226afd9f593e07d
junsuPark <krpjs0508@gmail.com>
2023-09-23 18:47:07 +0900

    ⚡️:: 임시 typealias 추가

--- core/core-datastore/src/main/java/com/xquare/core/datastore/Alias.kt
+++ core/core-datastore/src/main/java/com/xquare/core/datastore/Alias.kt
@@ -4,3 +4,9 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 
 typealias PreferencesDataStore = DataStore<Preferences>
+
+typealias Dms = String
+
+typealias Xquare = Dms
+
+typealias Android = Xquare
