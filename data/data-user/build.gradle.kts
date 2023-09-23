dd01e25c8e58ce6b0b3701cbd6a77713265ce93b
junsuPark <krpjs0508@gmail.com>
2023-09-23 17:01:39 +0900

    ♻️:: data-user 모듈 리팩토링

--- data/data-user/build.gradle.kts
+++ data/data-user/build.gradle.kts
@@ -1,60 +1,60 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+	alias(libs.plugins.androidLibrary)
+	alias(libs.plugins.kotlinAndroid)
 }
 
 android {
-    namespace = "com.xquare.data.user"
-    compileSdk = 34
+	namespace = "com.xquare.data.user"
+	compileSdk = 34
 
-    defaultConfig {
-        minSdk = 23
+	defaultConfig {
+		minSdk = 23
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		consumerProguardFiles("consumer-rules.pro")
+	}
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    kotlin {
-        jvmToolchain(8)
-    }
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro",
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
+	kotlin {
+		jvmToolchain(8)
+	}
 }
 
 dependencies {
 
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+	implementation(libs.core.ktx)
+	implementation(libs.appcompat)
+	implementation(libs.material)
+	testImplementation(libs.junit)
+	androidTestImplementation(libs.androidx.test.ext.junit)
+	androidTestImplementation(libs.espresso.core)
 
-    implementation(libs.koin.android)
-    implementation(libs.koin.android.compat)
-    testImplementation(libs.koin.android.test)
+	implementation(libs.koin.android)
+	implementation(libs.koin.android.compat)
+	testImplementation(libs.koin.android.test)
 
-    implementation(project(":database:database-user"))
+	implementation(project(":database:database-user"))
 
-    implementation(project(":datastore:datastore-user"))
+	implementation(project(":datastore:datastore-user"))
 
-    implementation(project(":network:network-user"))
+	implementation(project(":network:network-user"))
 
-    implementation(project(":shared:shared-alias"))
-    implementation(project(":shared:shared-date"))
+	implementation(project(":shared:shared-alias"))
+	implementation(project(":shared:shared-date"))
 }
