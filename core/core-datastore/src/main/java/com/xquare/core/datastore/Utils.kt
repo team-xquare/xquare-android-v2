2615a318d9bf281c200dc8cdd6a1a0ca99fad68f
junsuPark <krpjs0508@gmail.com>
2023-09-24 20:59:41 +0900

    ⚡️:: datastore util 구현

---
+++ core/core-datastore/src/main/java/com/xquare/core/datastore/Utils.kt
@@ -0,0 +1,13 @@
+package com.xquare.core.datastore
+
+import com.xquare.core.datastore.exception.TransformFailureException
+
+suspend inline fun <reified T> transform(
+	crossinline job: suspend () -> T,
+	onSuccess: (T) -> Unit = {},
+	onFailure: (Throwable) -> Unit = { throw TransformFailureException() },
+) =
+	runCatching { job() }
+		.onSuccess(onSuccess)
+		.onFailure(onFailure)
+		.getOrThrow()
